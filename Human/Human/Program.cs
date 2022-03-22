8dc2465238f06783f5a6755234dab0b2a0c3ecbd
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-22 15:53:21 +0530

    All

---
+++ Human/Human/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Human
+{
+    internal class Human
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Human can speak");
+        }
+    }
+}

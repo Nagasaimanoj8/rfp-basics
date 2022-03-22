11315699f5a0d726738a0104d211e1c53a2da9df
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-22 14:49:24 +0530

    all

---
+++ evenorodd/evenorodd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenorodd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter the Number :");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("{0} is Even Number", num);
+            else
+                Console.WriteLine("{0} is Odd Number", num);
+        }
+    }
+}

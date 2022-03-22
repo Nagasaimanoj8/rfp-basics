a931d3eaf796a0c40fc1f5b07776092e5a2cbc20
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-22 15:48:42 +0530

    reverse

---
+++ reverse of number/reverse of number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_of_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the num");
+            int perNum = int.Parse(Console.ReadLine());
+            int rev = 0;
+            while (perNum > 0)
+            {
+                int rem = perNum % 10;
+                rev = (rev * 10) + rem;
+                perNum = perNum / 10;
+            }
+            Console.WriteLine("reverse number is:" + rev);
+        }
+    }
+}
